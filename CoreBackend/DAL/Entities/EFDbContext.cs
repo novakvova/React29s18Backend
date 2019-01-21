c35dbd9bbb09daf49bd5d8472f81df18ead7a277
novakvova <novakvova@gmail.com>
2019-01-21 18:41:03 +0200

    Begin web site ASP.NET

---
+++ CoreBackend/DAL/Entities/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBackend.DAL.Entities
+{
+    public class EFDbContext : IdentityDbContext<IdentityUser>
+    {
+        public EFDbContext(DbContextOptions<EFDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
